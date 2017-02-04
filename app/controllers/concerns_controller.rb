c8be6e400caceff5b6d819f4a4d1d608a0e699f6
Bryan <Bpadron2000@gmail.com>
2017-02-03 20:12:52 -0500

    first page

---
+++ app/controllers/concerns_controller.rb
@@ -0,0 +1,4 @@
+class ConcernsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get 'concerns/index'
+
+  get 'benefits/index'
+
+  get 'uses/index'
+
+  get 'technologies/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
