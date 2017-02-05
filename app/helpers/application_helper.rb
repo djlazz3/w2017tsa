c83248647b83e56030192762c483a71129227e53
Bryan <Bpadron2000@gmail.com>
2017-02-04 23:51:37 -0500

    gradient background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-    def randomized_background_image
-      images = [image_url("backgroundblue.png"),image_url("backgroundred.png"),image_url("backgroundgreen.png"),image_url("backgroundwhite.png"),image_url("backgroundorange.png"),image_url("backgroundorange.png")]
-      images[rand(images.size)]
-    end
 end
